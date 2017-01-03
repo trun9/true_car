190b48469e6feef920545eb291ac21df8705c343
trun9 <trung86nguyen@msn.com>
2017-01-03 12:58:17 -0800

    Cleaned up test file

--- test.rb
+++ test.rb
@@ -11,86 +11,48 @@ class BasicTest < Minitest::Test
 
   def test_simple_object
     my_simple_object = {
-
         key1: 'value 1',
-
         key2: 3,
-
         key3: ['a', 'b', 'c'],
-
     }
 
-
-
     my_simple_object_2 = {
-
         key1: 'value 100',
-
         key2: 30,
-
         key3: ['c', 'b', 'a'],
-
     }
     assert(!self.identical?(my_simple_object, my_simple_object_2))
   end
 
   def test_complex_object
     my_complex_object = [
-
         {
-
             key1: 'val1',
-
             key2: 'val2',
-
             key3: [
-
                 {
-
                     subkey_1: ['a', 'b', 'c'],
-
                     subkey_2: "some value",
-
                 },
-
                 {
-
                     subkey_1: ['d', 'e', 'f'],
-
                     subkey_2: "another value",
-
                 }
-
             ]
-
         },
-
         {
-
             key1: 'val3',
-
             key2: 'val4',
-
             key3: [
-
                 {
-
                     subkey_1: ['g', 'h', 'i'],
-
                     subkey_2: "a different value",
-
                 },
-
                 {
-
                     subkey_1: ['j', 'k', 'l'],
-
                     subkey_2: "value",
-
                 }
-
             ]
-
         },
 
     ]
@@ -99,122 +61,64 @@ class BasicTest < Minitest::Test
 
   def test_complex_object_with_failure
     my_complex_object = [
-
         {
-
             key1: 'val1',
-
             key2: 'val2',
-
             key3: [
-
                 {
-
                     subkey_1: ['a', 'b', 'c'],
-
                     subkey_2: "some value",
-
                 },
-
                 {
-
                     subkey_1: ['d', 'e', 'f'],
-
                     subkey_2: "another value",
-
                 }
-
             ]
-
         },
-
         {
-
             key1: 'val3',
-
             key2: 'val4',
-
             key3: [
-
                 {
-
                     subkey_1: ['g', 'h', 'i'],
-
                     subkey_2: "a different value",
-
                 },
-
                 {
-
                     subkey_1: ['j', 'k', 'l'],
-
                     subkey_2: "value",
-
                 }
-
             ]
-
         },
-
     ]
     my_complex_object2 = [
-
         {
-
             key1: 'val1',
-
             key2: 'val2',
-
             key3: [
-
                 {
-
                     subkey_1: ['a', 'b', 'c'],
-
                     subkey_2: "some value",
-
                 },
-
                 {
-
                     subkey_1: ['d', 'e', 'f'],
-
                     subkey_2: "another value",
-
                 }
-
             ]
-
         },
-
         {
-
             key1: 'val3',
-
             key2: 'val4',
-
             key3: [
-
                 {
-
                     subkey_1: ['g', 'h', 'i'],
-
                     subkey_2: "a different value",
-
                 },
-
                 {
-
                     subkey_1: ['j', 'k', 'l'],
-
                     subkey_2: "false",
-
                 }
-
             ]
-
         },
-
     ]
     assert(!self.identical?(my_complex_object, my_complex_object2))
   end
